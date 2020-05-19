443fd22ff3e56e32c85d76b327496f1ecaa73f68
ed-commits <ed@github.com>
2020-05-19 14:16:15 +0100

    Get route_number and distination

--- bus.rb
+++ bus.rb
@@ -1,4 +1,6 @@
 class Bus
+  attr_reader :route_number, :distination
+
   def initialize(route_number, distination)
     @route_number = route_number
     @distination = distination
--- specs/bus_spec.rb
+++ specs/bus_spec.rb
@@ -4,4 +4,12 @@ require_relative("../bus")
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 class TestBus < MiniTest::Test
+  def setup()
+    @bus1 = Bus.new(22, "Ocean Terminal")
+  end
+
+  def test_create_bus()
+    assert_equal(22, @bus1.route_number)
+    assert_equal("Ocean Terminal", @bus1.distination)
+  end
 end
