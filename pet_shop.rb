cdf940d60f6a26f06accdb81fe6e8eeadb409077
Ed <ed-commits@users.noreply.github.com>
2020-05-19 11:21:21 +0100

    implement basic pet shop class

---
+++ pet_shop.rb
@@ -0,0 +1,11 @@
+class PetShop
+
+    attr_accessor :name, :pets, :total_cash
+
+    def initialize(name, pets, balance)
+        @name = name
+        @pets = pets
+        @total_cash = balance
+    end
+
+end
